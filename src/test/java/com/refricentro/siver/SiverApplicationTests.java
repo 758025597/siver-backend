98620e3bafccb465b6661cf8321d826ccff12844
Ruth Ale Alvarez Surco <ale.alv2006surco@gmail.com>
2026-09-01 15:18:14 -0500

    Estructura inicial backend Spring Boot

---
+++ src/test/java/com/refricentro/siver/SiverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.refricentro.siver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SiverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
